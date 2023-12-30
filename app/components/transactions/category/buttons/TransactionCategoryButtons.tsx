7d4a5f7a7613d0bb79fe0dc70993586b1abf2151
Stanislav Tugovikov <latrekc@fb.com>
2023-12-30 14:19:51 +0000

    Extract Edit button

--- app/components/transactions/category/buttons/TransactionCategoryButtons.tsx
+++ app/components/transactions/category/buttons/TransactionCategoryButtons.tsx
@@ -1,10 +1,7 @@
-import { Button, ButtonGroup, Input } from "@nextui-org/react";
-import { FormEvent, useCallback, useContext, useMemo, useState } from "react";
-import { TiEdit } from "react-icons/ti";
-import { graphql, useFragment, useMutation } from "react-relay";
-import { TransactionsCategoriesContext } from "../../TransactionsContext";
+import { ButtonGroup } from "@nextui-org/react";
+import { graphql, useFragment } from "react-relay";
 import TransactionCategoryDeleteButton from "./TransactionCategoryDeleteButton";
-import { TransactionCategoryButtonsEditMutation } from "./__generated__/TransactionCategoryButtonsEditMutation.graphql";
+import TransactionCategoryEditButton from "./TransactionCategoryEditButton";
 import { TransactionCategoryButtons_category$key } from "./__generated__/TransactionCategoryButtons_category.graphql";
 
 export default function TransactionCategoryButtons({
@@ -12,118 +9,20 @@ export default function TransactionCategoryButtons({
 }: {
   category: TransactionCategoryButtons_category$key;
 }) {
-  const { refetchCategories } = useContext(TransactionsCategoriesContext);
-  const [editMode, setEditMode] = useState(false);
-
   const category = useFragment(
     graphql`
       fragment TransactionCategoryButtons_category on Category {
-        id
-        name
-        parentCategory {
-          id
-        }
         ...TransactionCategoryDeleteButton_category
+        ...TransactionCategoryEditButton_category
       }
     `,
     category$key,
   );
 
-  const [commitEditMutation, isEditMutationInFlight] =
-    useMutation<TransactionCategoryButtonsEditMutation>(graphql`
-      mutation TransactionCategoryButtonsEditMutation(
-        $id: ID!
-        $name: String!
-        $parent: ID
-      ) {
-        updateCategory(id: $id, name: $name, parent: $parent) {
-          ... on Error {
-            error: message
-          }
-        }
-      }
-    `);
-
-  const [value, setValue] = useState(category.name);
-  const [error, setError] = useState<Error | null>(null);
-
-  const variant = useMemo(() => {
-    return value.length > 0 && value.trim() != category.name
-      ? "bordered"
-      : "flat";
-  }, [value]);
-
-  const onStartEdit = useCallback(() => {
-    setValue(value);
-    setEditMode(true);
-  }, [value]);
-
-  const onEdit = useCallback(
-    (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-
-      if (value.trim().length > 0 && value.trim() != category.name) {
-        commitEditMutation({
-          variables: {
-            id: category.id,
-            name: value,
-            parent: category?.parentCategory?.id,
-          },
-          onError(serverError) {
-            setError(serverError);
-          },
-          onCompleted(result) {
-            if (result.updateCategory.error) {
-              setError(new Error(result.updateCategory.error));
-            } else {
-              setValue(value);
-              setError(null);
-              setEditMode(false);
-              refetchCategories();
-            }
-          },
-        });
-      }
-    },
-    [value],
-  );
-
-  const onClear = useCallback(() => {
-    setEditMode(false);
-  }, []);
-
-  return editMode ? (
-    <form onSubmit={onEdit}>
-      <Input
-        label={
-          category.parentCategory != null ? "Edit subcategory" : "Edit category"
-        }
-        labelPlacement="inside"
-        className="p-4"
-        isClearable
-        onClear={onClear}
-        isDisabled={isEditMutationInFlight}
-        isInvalid={error != null}
-        errorMessage={error?.message}
-        size="sm"
-        value={value}
-        variant={variant}
-        onValueChange={setValue}
-      />
-    </form>
-  ) : (
+  return (
     <ButtonGroup className="invisible group-hover:visible">
       <TransactionCategoryDeleteButton category={category} />
-      <Button
-        size="sm"
-        variant="solid"
-        radius="full"
-        title="Edit category"
-        startContent={<TiEdit size="2em" />}
-        onClick={onStartEdit}
-      >
-        Edit
-      </Button>
+      <TransactionCategoryEditButton category={category} />
     </ButtonGroup>
   );
 }
---
+++ app/components/transactions/category/buttons/TransactionCategoryEditButton.tsx
@@ -0,0 +1,140 @@
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { FormEvent, useCallback, useContext, useMemo, useState } from "react";
+import { TiEdit } from "react-icons/ti";
+import { graphql, useFragment, useMutation } from "react-relay";
+import { TransactionsCategoriesContext } from "../../TransactionsContext";
+import { TransactionCategoryEditButtonMutation } from "./__generated__/TransactionCategoryEditButtonMutation.graphql";
+import { TransactionCategoryEditButton_category$key } from "./__generated__/TransactionCategoryEditButton_category.graphql";
+
+export default function TransactionCategoryEditButton({
+  category: category$key,
+}: {
+  category: TransactionCategoryEditButton_category$key;
+}) {
+  const { refetchCategories } = useContext(TransactionsCategoriesContext);
+
+  const category = useFragment(
+    graphql`
+      fragment TransactionCategoryEditButton_category on Category {
+        id
+        name
+        parentCategory {
+          id
+        }
+      }
+    `,
+    category$key,
+  );
+
+  const [commitEditMutation, isEditMutationInFlight] =
+    useMutation<TransactionCategoryEditButtonMutation>(graphql`
+      mutation TransactionCategoryEditButtonMutation(
+        $id: ID!
+        $name: String!
+        $parent: ID
+      ) {
+        updateCategory(id: $id, name: $name, parent: $parent) {
+          ... on Error {
+            error: message
+          }
+        }
+      }
+    `);
+
+  const [value, setValue] = useState(category.name);
+  const [error, setError] = useState<Error | null>(null);
+
+  const variant = useMemo(() => {
+    return value.length > 0 && value.trim() != category.name
+      ? "bordered"
+      : "flat";
+  }, [value]);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpenChange = useCallback(
+    (open: boolean) => {
+      setIsOpen(open);
+      setValue(category.name);
+      setError(null);
+    },
+    [value],
+  );
+
+  const onEdit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (value.trim().length > 0 && value.trim() != category.name) {
+        commitEditMutation({
+          variables: {
+            id: category.id,
+            name: value,
+            parent: category?.parentCategory?.id,
+          },
+          onError(serverError) {
+            setError(serverError);
+          },
+          onCompleted(result) {
+            if (result.updateCategory.error) {
+              setError(new Error(result.updateCategory.error));
+            } else {
+              setValue(value);
+              setError(null);
+              setIsOpen(false);
+              refetchCategories();
+            }
+          },
+        });
+      }
+    },
+    [value],
+  );
+
+  return (
+    <Popover showArrow onOpenChange={onOpenChange} isOpen={isOpen}>
+      <PopoverTrigger>
+        <Button
+          size="sm"
+          variant="solid"
+          radius="full"
+          title="Edit category"
+          isIconOnly
+        >
+          <TiEdit size="2em" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[240px]">
+        {() => (
+          <div className="w-full p-4">
+            <form onSubmit={onEdit}>
+              <Input
+                label={
+                  category.parentCategory != null
+                    ? "Edit subcategory"
+                    : "Edit category"
+                }
+                autoFocus
+                labelPlacement="inside"
+                className="p-4"
+                isDisabled={isEditMutationInFlight}
+                isInvalid={error != null}
+                errorMessage={error?.message}
+                size="sm"
+                value={value}
+                variant={variant}
+                onValueChange={setValue}
+              />
+            </form>
+          </div>
+        )}
+      </PopoverContent>
+    </Popover>
+  );
+}
