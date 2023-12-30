6e8e74cc641403b2d0991f2f563afb73fb2a97c2
Stanislav Tugovikov <latrekc@fb.com>
2023-12-30 13:53:32 +0000

    Extract remove category button and add confirmation

---
+++ app/components/transactions/category/TransactionCategoryDeleteButton.tsx
@@ -0,0 +1,109 @@
+import {
+  Button,
+  ButtonGroup,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Spacer,
+} from "@nextui-org/react";
+import { useCallback, useContext } from "react";
+import { TiDelete } from "react-icons/ti";
+import { graphql, useFragment, useMutation } from "react-relay";
+import { TransactionsCategoriesContext } from "../TransactionsContext";
+import { TransactionCategoryDeleteButtonMutation } from "./__generated__/TransactionCategoryDeleteButtonMutation.graphql";
+import { TransactionCategoryDeleteButton_category$key } from "./__generated__/TransactionCategoryDeleteButton_category.graphql";
+
+export default function TransactionCategoryDeleteButton({
+  category: category$key,
+}: {
+  category: TransactionCategoryDeleteButton_category$key;
+}) {
+  const { refetchCategories } = useContext(TransactionsCategoriesContext);
+
+  const category = useFragment(
+    graphql`
+      fragment TransactionCategoryDeleteButton_category on Category {
+        id
+        name
+        parentCategory {
+          name
+        }
+      }
+    `,
+    category$key,
+  );
+
+  const [commitDeleteMutation, isDeleteMutationInFlight] =
+    useMutation<TransactionCategoryDeleteButtonMutation>(graphql`
+      mutation TransactionCategoryDeleteButtonMutation($id: ID!) {
+        deleteCategory(id: $id) {
+          ... on Error {
+            error: message
+          }
+        }
+      }
+    `);
+
+  const onDelete = useCallback(() => {
+    if (category == null) {
+      alert("Null category");
+      return;
+    }
+    commitDeleteMutation({
+      variables: {
+        id: category.id,
+      },
+      onCompleted(result) {
+        if (result.deleteCategory.error) {
+          alert(result.deleteCategory.error);
+        } else {
+          refetchCategories();
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <Popover showArrow>
+      <PopoverTrigger>
+        <Button
+          color="danger"
+          size="sm"
+          variant="solid"
+          radius="full"
+          title="Remove category"
+          isIconOnly
+        >
+          <TiDelete color="white" size="2em" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[240px]">
+        {() => (
+          <div className="w-full p-4">
+            <p className="text-small text-foreground">
+              Are sure you want to remove <b>{category.name}</b> from{" "}
+              {category.parentCategory == null ? (
+                "a root category"
+              ) : (
+                <b>{category.parentCategory.name}</b>
+              )}
+              ?
+            </p>
+
+            <Spacer />
+
+            <ButtonGroup>
+              <Button
+                isDisabled={isDeleteMutationInFlight}
+                onClick={onDelete}
+                color="danger"
+              >
+                Yes, remove
+              </Button>
+            </ButtonGroup>
+          </div>
+        )}
+      </PopoverContent>
+    </Popover>
+  );
+}
