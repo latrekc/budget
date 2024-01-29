2656256e6ad3630f24a139112f1c53a7ce1b73cf
Stanislav Tugovikov <latrekc@fb.com>
2024-01-29 21:29:49 +0000

    Filter by income only

--- app/components/transactions/TransactionsFilters.tsx
+++ app/components/transactions/TransactionsFilters.tsx
@@ -1,13 +1,14 @@
 import { Dispatch } from "react";
 import { graphql, useFragment } from "react-relay";
 
+import { FiltersState, ReducerAction } from "./TransactionsFiltersReducer";
+import { TransactionsSelection } from "./TransactionsTable";
+import TransactionsTotal from "./TransactionsTotal";
 import { TransactionsFilters$key } from "./__generated__/TransactionsFilters.graphql";
 import TransactionCategoriesFilter from "./filter/TransactionCategoriesFilter";
 import TransactionComplitedFilter from "./filter/TransactionComplitedFilter";
 import TransactionDescriptionFilter from "./filter/TransactionDescriptionFilter";
-import { FiltersState, ReducerAction } from "./TransactionsFiltersReducer";
-import { TransactionsSelection } from "./TransactionsTable";
-import TransactionsTotal from "./TransactionsTotal";
+import TransactionIncomeFilter from "./filter/TransactionIncomeFilter";
 
 export default function TransactionsFilters({
   data: data$key,
@@ -39,6 +40,7 @@ export default function TransactionsFilters({
 
       <div className="m-6 flex flex-row flex-wrap gap-x-3">
         <TransactionComplitedFilter dispatch={dispatch} filters={filters} />
+        <TransactionIncomeFilter dispatch={dispatch} filters={filters} />
         <TransactionCategoriesFilter
           dispatch={dispatch}
           filters={filters}
