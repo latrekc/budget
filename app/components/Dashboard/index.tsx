dbef4c707d83dc40362a25902846db118a4011f9
Stanislav Tugovikov <latrekc@meta.com>
2024-06-09 23:06:39 +0100

    Drop obsolete period filtering

--- app/components/Dashboard/index.tsx
+++ app/components/Dashboard/index.tsx
@@ -1,63 +1,29 @@
-import { Select, SelectItem } from "@nextui-org/react";
-import { useCallback, useState } from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
 
+import Filters from "../Filters";
+import DashboardByTimePeriods from "./DashboardByTimePeriods";
 import { DashboardQuery } from "./__generated__/DashboardQuery.graphql";
-import DashboardByTimePeriods, { Period } from "./DashboardByTimePeriods";
 
 export default function Dashboard() {
-  const periods = [
-    { label: "Years", value: Period.Years },
-    { label: "Months", value: Period.Months },
-  ];
-  const [period, setPeriod] = useState<Period>(Period.Months);
-
-  const onSelectionChange = useCallback((keys: "all" | Set<React.Key>) => {
-    if (keys instanceof Set) {
-      setPeriod([...keys.values()].map((k) => k.toString())[0] as Period);
-    }
-
-    return keys;
-  }, []);
-
   const data = useLazyLoadQuery<DashboardQuery>(
     graphql`
-      query DashboardQuery(
-        $include_months: Boolean!
-        $include_years: Boolean!
-      ) {
+      query DashboardQuery {
+        ...Filters
         ...DashboardByTimePeriods
-          @arguments(
-            include_months: $include_months
-            include_years: $include_years
-          )
       }
     `,
-    {
-      include_months: period === Period.Months,
-      include_years: period === Period.Years,
-    },
+    {},
   );
 
   return (
-    <div className="p-6">
-      <Select
-        className="max-w-xs"
-        label="Stat by"
-        onSelectionChange={onSelectionChange}
-        placeholder="Select a period"
-        selectedKeys={[period.toString()]}
-        size="lg"
-        variant="bordered"
-      >
-        {periods.map((period) => (
-          <SelectItem key={period.value} value={period.value}>
-            {period.label}
-          </SelectItem>
-        ))}
-      </Select>
+    <div className="flex flex-row">
+      <div className="basis-3/4 py-3">
+        <DashboardByTimePeriods statistic={data} />
+      </div>
 
-      <DashboardByTimePeriods period={period} statistic={data} />
+      <div className="basis-1/4 p-6">
+        <Filters data={data} />
+      </div>
     </div>
   );
 }
