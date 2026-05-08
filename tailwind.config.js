34fc2aa340b5803acc2bd6d381f3d9dede6a6263
Stanislav Tugovikov <stanislav@tugovikov.com>
2026-05-08 01:13:09 +0100

    Fix building CSS for nextui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/**/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/.pnpm/@nextui-org+theme@*/node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [nextui()],
   theme: {
